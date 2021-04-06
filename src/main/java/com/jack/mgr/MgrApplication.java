b7df988d503faa57ff2efc11aae5734aba0f6e39
zhengzhe17 <zhengzhe17@jd.com>
2021-04-06 18:37:40 +0800

    feat: update

--- src/main/java/com/jack/mgr/MgrApplication.java
+++ src/main/java/com/jack/mgr/MgrApplication.java
@@ -1,9 +1,12 @@
 package com.jack.mgr;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.jack.mgr.mapper")
 public class MgrApplication {
 
 	public static void main(String[] args) {
