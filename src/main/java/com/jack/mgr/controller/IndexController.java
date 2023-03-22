621289b24f517a491e83223fff8dc99da082e0b7
zhengzhe17 <zhengzhe17@jd.com>
2023-03-22 16:31:27 +0800

    add log converter and logback config

--- src/main/java/com/jack/mgr/controller/IndexController.java
+++ src/main/java/com/jack/mgr/controller/IndexController.java
@@ -1,9 +1,16 @@
 package com.jack.mgr.controller;
 
+import com.jack.mgr.utils.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * IndexController
@@ -24,6 +31,15 @@ public class IndexController {
     @RequestMapping(value = "/test")
     public String test(Model model, Long id) {
         log.info("got {}", id);
+        return "";
+    }
+
+    @RequestMapping(value = "/postTest",
+        method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseBody
+    public String postTest(@RequestParam Map<String, Object> parmas) {
+        log.info("post test got {}", JsonUtils.toJson(parmas));
         return "";
     }
 }
